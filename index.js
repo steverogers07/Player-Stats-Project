cbf2119a51ce829c955bc437237bb4a5e6392ce2
steverogers07 <dvas325@gmail.com>
2020-03-17 01:46:18 +0530

    Some Changes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function fun(){
 	var side=document.getElementById("sidebar");
-	if(side.style.display==="none"){
-		side.style.display="block";
-	}
-	else{
-		side.style.display="none";
-	}
+		side.style.left="0";
+	var butt=document.querySelector(".shifttab");
+	console.log(butt);
+	butt.classList.add("shifttabtog");
+	butt.classList.remove("shifttabtog2");
+	};
+function fun2(){
+	var side=document.getElementById("sidebar");
+		side.style.left="-300px";
+	var butt=document.querySelector(".shifttab");
+	console.log(butt);
+	butt.classList.remove("shifttabtog");
+	butt.classList.add("shifttabtog2");	
 };
